2e1e025f1dd93c5ed38df9508622d6ea2fd1b627
정이든 <2donny@naver.com>
2021-09-14 05:05:17 +0900

    Pipe : Validation options

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as logger from 'morgan';
 import { ValidationPipe } from '@nestjs/common';
+import * as moment from 'moment';
+import 'moment/locale/ko';
+import { NextFunction, Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +12,13 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
-  app.useGlobalPipes(new ValidationPipe());
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
