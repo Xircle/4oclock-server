f6a67819ffae40f5f42755674718fba4f1ac5909
정이든 <2donny@naver.com>
2021-09-15 18:22:00 +0900

    AuthUser decorator

--- src/auth/auth-user.decorator.ts
+++ src/auth/auth-user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetUser = createParamDecorator(
+export const AuthUser = createParamDecorator(
   (data: any, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     return req.user;
