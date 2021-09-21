1e87648d39522245026a1028bab15b84bff2cb91
정이든 <2donny@naver.com>
2021-09-21 21:32:43 +0900

    auth module

--- src/auth/auth-user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const AuthUser = createParamDecorator(
-  (data: any, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,35 +1,24 @@
 import { UserProfile } from '../user/entities/user-profile.entity';
 import { JwtStrategy } from './jwt.strategy';
 import { User } from './../user/entities/user.entity';
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService, SocialAuthService } from './auth.service';
 import { AuthController, SocialAuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { config } from 'dotenv';
 import SocialAccount from 'src/user/entities/social-account.entity';
 import { S3Service } from 'src/aws/s3/s3.service';
 
-config();
-
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, SocialAccount, UserProfile]),
     PassportModule.register({
       defaultStrategy: 'jwt',
     }),
     JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: {
-        expiresIn: 60 * 60 * 3, // Token expires in 1 hour
-      },
+      secret: process.env.JWT_SECRET_KEY!,
     }),
+    TypeOrmModule.forFeature([User, SocialAccount, UserProfile]),
   ],
   providers: [AuthService, SocialAuthService, JwtStrategy, S3Service],
   controllers: [AuthController, SocialAuthController],
