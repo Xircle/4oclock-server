5cc9b63760a9fc5b0cd5bf1b480ee5598bac7216
정이든 <2donny@naver.com>
2021-09-26 23:05:22 +0900

    web socket 추가

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,35 @@
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway(80, { namespace: /\/ws-.+/, transports: ['websocket'] })
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() public server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: string): string {
+    console.log(data);
+    return data;
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('init');
+  }
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected : ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected : ${client.id}`);
+  }
+}
