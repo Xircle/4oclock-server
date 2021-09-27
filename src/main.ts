22e57990253d30ca73b60ea9d9af235fd7d4960f
정이든 <2donny@naver.com>
2021-09-27 19:14:20 +0900

    @nestjs/swagger document

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as logger from 'morgan';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,23 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 5000);
+
+  const config = new DocumentBuilder()
+    .setTitle('네시모해 API')
+    .setDescription('네시모해 개발을 위한 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'jwt',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.SERVER_PORT || 3080);
 }
 bootstrap();
