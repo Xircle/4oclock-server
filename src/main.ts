3661dbb50cdaaa4f4f7c25f32affb5210accf2a5
정이든 <2donny@naver.com>
2021-09-28 05:05:37 +0900

    tes

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger('dev'));
   app.enableCors({
-    origin: ['https://xircle.netlify.app', 'http://localhost:300'],
+    origin: ['https://xircle.netlify.app', 'http://localhost:3000'],
     credentials: true,
   });
   app.useGlobalPipes(
