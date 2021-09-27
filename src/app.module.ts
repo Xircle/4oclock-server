11f7db17b2a808c0ba7215c9b049551d9d6a276a
정이든 <2donny@naver.com>
2021-09-27 19:15:04 +0900

    url로 할 시 database name 적용 안되는 에러 fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,12 @@ import * as Joi from 'joi';
       isGlobal: true,
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').default('dev').required(),
-        DATABASE_URL: Joi.string().required(),
+        SERVER_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number(),
+        DB_NAME: Joi.string().required(),
         KAKAO_ID: Joi.string().required(),
         JWT_SECRET_KEY: Joi.string().required(),
         AWS_S3_BUCKET_NAME: Joi.string().required(),
