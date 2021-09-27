857dca9d137770c0b97390aabbc994b3a61279bb
정이든 <2donny@naver.com>
2021-09-27 21:58:49 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.SERVER_PORT || 3080);
+  await app.listen(process.env.PORT || 3080);
 }
 bootstrap();
