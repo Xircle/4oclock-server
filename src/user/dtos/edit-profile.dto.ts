c7c43c18547fe350176c54e32e0c714bb3e2aeb8
정이든 <2donny@naver.com>
2021-10-04 13:08:34 +0900

    Add: isYkClub 엔터티 추가

--- src/user/dtos/edit-profile.dto.ts
+++ src/user/dtos/edit-profile.dto.ts
@@ -4,6 +4,14 @@ import { SocialRegisterInput } from 'src/auth/dtos/social-register.dto';
 
 export class EditProfileInput extends PartialType(SocialRegisterInput) {
   @ApiProperty({
+    name: '연고이팅 회원이신가요?',
+    example: 'true',
+  })
+  @IsString()
+  @IsOptional()
+  isYkClub?: boolean;
+
+  @ApiProperty({
     name: '활동이력',
     example: '인사이더스 13기',
   })
