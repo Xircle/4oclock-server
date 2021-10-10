151757e12d79605fd31c12916e22107bcec1eebd
정이든 <2donny@naver.com>
2021-10-11 01:41:09 +0900

    Fix: edit place

--- src/place/dtos/edit-place.dto.ts
+++ src/place/dtos/edit-place.dto.ts
@@ -1,4 +1,17 @@
-import { CreatePlaceInput } from './create-place.dto';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
+import { Place } from 'src/place/entities/place.entity';
+import { PlaceDetail } from './../entities/place-detail.entity';
 
-export class EditPlaceInput extends PartialType(CreatePlaceInput) {}
+export class EditPlaceInput {
+  @ApiProperty({
+    example: '{ name: 매운탕집!, isClosed: true}',
+    description: 'Place 테이블에서 변경할 사항',
+  })
+  editedPlace: Partial<Place>;
+
+  @ApiProperty({
+    example: '{ title: 잊을수가 없다!, categories: [맥주, 호프]}',
+    description: 'PlaceDetail 테이블에서 변경할 사항',
+  })
+  editedPlaceDetail: Partial<PlaceDetail>;
+}
