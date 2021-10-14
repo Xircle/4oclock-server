d308af813b31f70d3e7ab4c1ec72cd7b65683f53
정이든 <2donny@naver.com>
2021-10-14 11:48:33 +0900

    DI

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,10 +1,11 @@
+import { RoomModule } from './../room/room.module';
 import { AuthModule } from './../auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
 import { ChatsService } from './chats.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RoomModule],
   providers: [ChatsGateway, ChatsService],
   exports: [ChatsGateway],
 })
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,14 +1,16 @@
+import { RoomRepository } from './repository/room.repository';
 import { AuthModule } from './../auth/auth.module';
 import { User } from 'src/user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
 import { Room } from './entities/room.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room, User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Room, User, RoomRepository]), AuthModule],
   providers: [RoomService],
   controllers: [RoomController],
+  exports: [RoomService],
 })
 export class RoomModule {}
