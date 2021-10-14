3289f76fc8477b2fd0a172d44df0a430da4a436e
정이든 <2donny@naver.com>
2021-10-14 11:49:17 +0900

    remove console

--- src/chats/guards/room.guard.ts
+++ src/chats/guards/room.guard.ts
@@ -9,11 +9,7 @@ export class RoomGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext) {
     const data = context.switchToWs().getData<RoomGuardDto>();
-    const client: Socket = context.switchToWs().getClient();
-    console.log(data);
-
     const existRoom = await this.roomService.getRoomById(data.roomId);
-    console.log(existRoom);
     if (
       !existRoom ||
       !existRoom.users.some((user) => user.id === data.senderId)
