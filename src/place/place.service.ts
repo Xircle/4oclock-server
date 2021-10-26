47516aeb87ce3e5bd72bc22034a24d09d7f7a0c2
정이든 <2donny@naver.com>
2021-10-26 17:11:02 +0900

    Persistance, Domain layer와 분리

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -12,14 +12,18 @@ import {
   CreatePlaceOutput,
   PlacePhotoInput,
 } from './dtos/create-place.dto';
-import { PlaceUtilService } from './../utils/place/place-util.service';
 import { PlaceDetail } from './entities/place-detail.entity';
 import { User } from './../user/entities/user.entity';
 import { DeletePlaceOutput } from './dtos/delete-place.dto';
 import { S3Service } from 'src/aws/s3/s3.service';
 import { GetPlacesByLocationOutput } from './dtos/get-place-by-location.dto';
 import { Place } from './entities/place.entity';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { getManager, Repository } from 'typeorm';
 import {
@@ -44,12 +48,20 @@ export class PlaceService {
     private reservationRepository: Repository<Reservation>,
     private placeRepository: PlaceRepository,
     private placeDetailRepository: PlaceDetailRepository,
-    private placeUtilService: PlaceUtilService,
     private reviewRepository: ReviewRepository,
     private s3Service: S3Service,
     private reservationUtilService: ReservationUtilService,
     private eventService: EventService,
   ) {}
+
+  async checkPlaceException(entity: Place) {
+    if (!entity) {
+      throw new HttpException(
+        '존재하지 않는 장소입니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 
   async getPlacesByLocation(
     anyUser: User | undefined,
@@ -132,15 +144,7 @@ export class PlaceService {
         const participants: MainFeedPlaceParticipantsProfile[] =
           await this.reservationUtilService.getParticipantsProfile(place.id);
 
-        let deadline = this.placeUtilService.getDeadlineCaption(
-          place.startDateAt,
-          place.isLightning,
-          place.startTime,
-        );
-        const startDateFromNow = this.placeUtilService.getEventDateCaption(
-          place.startDateAt,
-        );
-
+        const deadline = place.getDeadlineCaption();
         // isClosed Update 로직
         if (place.isLightning) {
           if (deadline === '번개 마감' && !place.isClosed) {
@@ -153,6 +157,7 @@ export class PlaceService {
             await this.placeRepository.savePlace(place);
           }
         }
+        const startDateFromNow = place.getStartDateFromNow();
         mainFeedPlaces.push({
           ...place,
           isParticipating,
@@ -184,9 +189,10 @@ export class PlaceService {
     placeId: string,
   ): Promise<GetPlaceByIdOutput> {
     try {
-      const place = await this.placeRepository.findPlaceByIdAndCheckException(
+      const place = await this.placeRepository.findDetailPlaceByPlaceId(
         placeId,
       );
+      if (!place) this.checkPlaceException(place);
 
       // 조회수 업데이트
       await this.placeRepository.updatePlace(
@@ -198,10 +204,6 @@ export class PlaceService {
         },
       );
 
-      // 이벤트 시작 시간
-      const startDateFromNow = this.placeUtilService.getEventDateCaption(
-        place.startDateAt,
-      );
       // 참여 여부
       let isParticipating = false;
       if (anyUser) {
@@ -216,6 +218,7 @@ export class PlaceService {
           isCanceled: false,
         },
       });
+
       // 참여자 성비, 평균 나이 추가
       const participants: PlaceDataParticipantsProfile[] =
         await this.reservationUtilService.getParticipantsProfile(placeId);
@@ -234,6 +237,8 @@ export class PlaceService {
         average_age: Math.floor(sum_age / total_count) || 0,
       };
 
+      // 이벤트 시작 시간
+      const startDateFromNow = place.getStartDateFromNow();
       const placeData: PlaceData = {
         ...place,
         reviews: place.reviews,
@@ -377,17 +382,8 @@ export class PlaceService {
   ): Promise<CoreOutput> {
     const { editedPlace, editedPlaceDetail } = editPlaceInput;
     try {
-      const exists = await this.placeRepository.findOne({
-        where: {
-          id: placeId,
-        },
-      });
-      if (!exists) {
-        return {
-          ok: false,
-          error: '존재하지 않는 장소입니다.',
-        };
-      }
+      const place = await this.placeRepository.findOneByPlaceId(placeId);
+      this.checkPlaceException(place);
 
       if (_.isEqual(editPlaceInput, {})) {
         return {
@@ -434,11 +430,7 @@ export class PlaceService {
     editPlaceReviewImagesInput: EditPlaceReviewImagesInput,
   ): Promise<CoreOutput> {
     try {
-      const placeExist = await this.placeRepository.findOne({
-        where: {
-          id: placeId,
-        },
-      });
+      const placeExist = await this.placeRepository.findOneByPlaceId(placeId);
       const reviewExist = await this.reviewRepository.findOne({
         where: {
           id: reviewId,
@@ -487,17 +479,8 @@ export class PlaceService {
     placeId: string,
   ): Promise<GetPlaceParticipantListOutput> {
     try {
-      const exists = await this.placeRepository.find({
-        where: {
-          id: placeId,
-        },
-      });
-      if (!exists) {
-        return {
-          ok: false,
-          error: '존재하지 않는 장소입니다.',
-        };
-      }
+      const place = await this.placeRepository.findOneByPlaceId(placeId);
+      this.checkPlaceException(place);
 
       // 참가자들 간략한 프로필 정보
       const participantListProfiles: PlaceDataParticipantsProfile[] =
