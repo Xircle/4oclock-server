245d41137da4d3e9de40d89149acdd8a2a2b1eb4
정이든 <2donny@naver.com>
2021-11-10 15:23:15 +0900

    server code changed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { applyMiddleware } from './middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   applyMiddleware(app);
-  await app.listen(process.env.PORT || 3080);
+  await app.listen(process.env.PORT || 3080, () => {
+    process.send?.('ready');
+  });
+
+  process.on('SIGINT', () => {
+    console.log('process exited');
+    process.exit(0);
+  });
 }
 bootstrap();
