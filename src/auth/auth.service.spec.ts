bc4bdce52b300343d15e52834be97d4f05a3dee6
정이든 <2donny@naver.com>
2021-12-08 18:56:01 +0900

    [Unit test] profileImageUrl null일 때  s3Service.uploadToS3 실행 테스트

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm';
+import * as typeorm from 'typeorm';
 import { Test } from '@nestjs/testing';
 import { ConfigService } from '@nestjs/config';
 import { getRepositoryToken } from '@nestjs/typeorm';
@@ -11,11 +11,12 @@ import { SocialAccount } from '@user/entities/social-account.entity';
 import { JwtService } from '@nestjs/jwt';
 import { Readable } from 'stream';
 
-const mockUserProfileRepository = { create: jest.fn() };
-const mockSocialAccountRepository = { create: jest.fn() };
+const mockUserProfileRepository = { create: jest.fn(), save: jest.fn() };
+const mockSocialAccountRepository = { create: jest.fn(), save: jest.fn() };
 const mockUserRepository = {
   findOne: jest.fn(),
   create: jest.fn(),
+  save: jest.fn(),
 };
 const mockS3Service = {
   uploadToS3: jest.fn(),
@@ -26,13 +27,20 @@ const mockConfigService = {
 const mockJwtService = {
   sign: jest.fn(),
 };
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+type MockRepository<T = any> = Partial<
+  Record<keyof typeorm.Repository<T>, jest.Mock>
+>;
 
 describe('SocialAuthService', () => {
   let socialAuthService: SocialAuthService;
   let userRepository: MockRepository<User>;
+  let userProfileRepository: MockRepository<UserProfile>;
+  let socialAccountRepository: MockRepository<SocialAccount>;
+  let s3Service: S3Service;
+  let configService: ConfigService;
+  let jwtService: JwtService;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
         SocialAuthService,
@@ -62,8 +70,13 @@ describe('SocialAuthService', () => {
         },
       ],
     }).compile();
-    socialAuthService = module.get<SocialAuthService>(SocialAuthService);
+    socialAuthService = module.get(SocialAuthService);
     userRepository = module.get(UserRepository);
+    userProfileRepository = module.get(getRepositoryToken(UserProfile));
+    socialAccountRepository = module.get(getRepositoryToken(SocialAccount));
+    s3Service = module.get(S3Service);
+    configService = module.get(ConfigService);
+    jwtService = module.get(JwtService);
   });
 
   it('should be defined', () => {
@@ -71,46 +84,65 @@ describe('SocialAuthService', () => {
   });
 
   describe('socialRegister', () => {
+    const socialRegisterArgs = {
+      profileImageUrl: expect.any(String),
+      socialId: expect.any(String),
+      email: expect.any(String),
+      phoneNumber: expect.any(String),
+      gender: Gender.Male,
+      username: expect.any(String),
+      university: expect.any(String),
+      isGraduate: expect.any(Boolean),
+      age: expect.any(Number),
+      job: expect.any(String),
+      shortBio: expect.any(String),
+      location: expect.any(String),
+      personality: expect.any(String),
+      MBTI: expect.any(String),
+      drinkingStyle: expect.any(Number),
+      interests: [expect.any(String)],
+      isMarketingAgree: expect.any(Boolean),
+    };
+    const profileImageFileArgs = {
+      fieldname: '',
+      originalname: '',
+      mimetype: '',
+      size: 1,
+      stream: new Readable(),
+      destination: '',
+      encoding: '',
+      filename: '',
+      path: '',
+      buffer: Buffer.from(''),
+    };
+
+    it('should upload file to s3, if profileImageUrl does not exist', async () => {
+      userRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'she_lock@naver.com',
+      });
+      await socialAuthService.socialRegister(
+        { ...socialRegisterArgs, profileImageUrl: null },
+        profileImageFileArgs,
+        'kakao',
+      );
+      expect(s3Service.uploadToS3).toHaveBeenCalledTimes(1);
+      expect(s3Service.uploadToS3).toHaveBeenCalledWith(
+        profileImageFileArgs,
+        socialRegisterArgs.socialId,
+      );
+    });
+
     it('should fail, if user exists', async () => {
       userRepository.findOne.mockResolvedValue({
         id: 1,
         email: 'she_lock@naver.com',
       });
       const result = await socialAuthService.socialRegister(
-        {
-          profileImageUrl: expect.any(String),
-          socialId: expect.any(String),
-          email: expect.any(String),
-          phoneNumber: expect.any(String),
-          gender: Gender.Male,
-          username: expect.any(String),
-          university: expect.any(String),
-          isGraduate: expect.any(Boolean),
-          age: expect.any(Number),
-          job: expect.any(String),
-          shortBio: expect.any(String),
-          location: expect.any(String),
-          personality: expect.any(String),
-          MBTI: expect.any(String),
-          drinkingStyle: expect.any(Number),
-          interests: [expect.any(String)],
-          isMarketingAgree: expect.any(Boolean),
-        },
-        {
-          fieldname: '',
-          originalname: '',
-          mimetype: '',
-          size: 1,
-          stream: new Readable(),
-          destination: '',
-          encoding: '',
-          filename: '',
-          path: '',
-          buffer: Buffer.from(''),
-        },
+        socialRegisterArgs,
+        profileImageFileArgs,
         'kakao',
       );
-
       expect(result).toMatchObject({
         ok: false,
         error: '이미 존재하는 이메일입니다.',
@@ -124,6 +156,6 @@ describe('SocialAuthService', () => {
     });
   });
 
-  it.todo('socialRegister');
+  it.todo('socialRegister.transaction');
   it.todo('socialRedirect');
 });
