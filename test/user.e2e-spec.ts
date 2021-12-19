3f3dae4b4ec28406d4310b11cd4f68e8a26c246a
정이든 <2donny@naver.com>
2021-12-19 20:47:18 +0900

    [e2e test] UserController

---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,37 @@
+import * as request from 'supertest';
+import { getConnection } from 'typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  describe('me', () => {
+    test('should return 401 code, if user is Unauthorized', () => {
+      return request(app.getHttpServer())
+        .get('/user/me')
+        .expect(401)
+        .expect((res) => {
+          console.log(res);
+        });
+    });
+  });
+  test.todo('editProfile');
+  test.todo('seeRandomProfile');
+  test.todo('seeUserById');
+  test.todo('getMyPlace');
+});
