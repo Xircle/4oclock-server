d4cb5381753f135266ab2c6471511ea5b1ba4616
정이든 <2donny@naver.com>
2021-12-25 23:37:36 +0900

    [Add] Apply global Serialization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-import { INestApplication } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as logger from 'morgan';
@@ -20,6 +21,7 @@ export const applyMiddleware = (app: INestApplication) => {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
     .setTitle('네시모해(연고이팅) API')
