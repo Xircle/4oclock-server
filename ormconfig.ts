dc11a7cf23c78f69597ee17db82104391495662b
정이든 <2donny@naver.com>
2021-12-28 18:32:55 +0900

    Setup migrations, cli

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,18 +1,26 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import './src/env';
-
-export const ormconfig: TypeOrmModuleOptions = {
+const ormconfig: TypeOrmModuleOptions = {
   type: 'postgres',
   username: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   host: process.env.RDS_HOSTNAME,
   port: +process.env.RDS_PORT,
   database: process.env.RDS_DB_NAME,
-  entities: [__dirname + '/**/*.entity.{js,ts}'],
   ssl: false,
   synchronize: false,
   logging: process.env.NODE_ENV === 'dev',
   keepConnectionAlive: true,
   namingStrategy: new SnakeNamingStrategy(),
+  entities: [__dirname + '/**/*.entity.{js,ts}'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  migrations: [__dirname + '/database/migrations/**/*.ts'],
+  migrationsTableName: 'migrations',
+  cli: {
+    entitiesDir: __dirname + '/**/*.entity.{js,ts}',
+    migrationsDir: __dirname + '/database/migrations/',
+    subscribersDir: 'src/subscriber',
+  },
 };
+export default ormconfig;
