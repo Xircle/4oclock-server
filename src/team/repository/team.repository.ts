938e3ec532cc4afd5da2ad1ba5081920b3653ce3
dja1716 <hdum@uwaterloo.ca>
2022-02-08 17:09:46 +0900

    fetch: team orderby ASC"

--- src/team/repository/team.repository.ts
+++ src/team/repository/team.repository.ts
@@ -4,6 +4,10 @@ import { Team } from '../entities/team.entity';
 @EntityRepository(Team)
 export class TeamRepository extends Repository<Team> {
   public async findManyTeams(): Promise<Team[]> {
-    return this.find();
+    return this.find({
+      order: {
+        id: 'ASC',
+      },
+    });
   }
 }
