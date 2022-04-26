2896604ec923e1a328e85f0fb4723dfcb6c70609
dja1716 <hdum@uwaterloo.ca>
2022-04-26 19:47:44 +0900

    edit: edit place dto > subImageUrls added

--- src/place/dtos/edit-place.dto.ts
+++ src/place/dtos/edit-place.dto.ts
@@ -14,4 +14,11 @@ export class EditPlaceInput {
     description: 'PlaceDetail 테이블에서 변경할 사항',
   })
   editedPlaceDetail: Partial<PlaceDetail>;
+
+  @ApiProperty({
+    example:
+      '{https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-16943905-0a91-4684-93b7-e0979e005949-1650895713186-A265814F-E69F-49C0-A004-C7295A9191BC.jpg}',
+    description: 'subImageUrls',
+  })
+  subImageUrls: string[];
 }
