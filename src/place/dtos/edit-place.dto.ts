bb7ac2536b2b0c0d91378a5e228821ddb7e715f4
dja1716 <hdum@uwaterloo.ca>
2022-04-27 15:52:11 +0900

    edit: edit place dto

--- src/place/dtos/edit-place.dto.ts
+++ src/place/dtos/edit-place.dto.ts
@@ -20,5 +20,14 @@ export class EditPlaceInput {
       '{https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-16943905-0a91-4684-93b7-e0979e005949-1650895713186-A265814F-E69F-49C0-A004-C7295A9191BC.jpg}',
     description: 'subImageUrls',
   })
-  subImageUrls: string[];
+  oldSubImageUrls: string[];
+
+  @ApiProperty({
+    description: '이전 커버 이미지가 지워졌는지 판단',
+  })
+  isCoverImageDeleted: boolean;
+}
+
+export class EditPlacePhotoInput {
+  images?: Express.Multer.File[];
 }
