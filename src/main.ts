59cd2767607e72328f09b84b35d6ef9411278dd5
dja1716 <hdum@uwaterloo.ca>
2022-05-23 15:44:03 +0900

    firebase config

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { applyMiddleware } from './middleware';
 import 'env';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
 
 async function bootstrap() {
+  const firebaseConfig: ServiceAccount = {
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig),
+    databaseURL:
+      'aa1x1d93gqdhvlg.cxxbypwc85ck.ap-northeast-2.rds.amazonaws.com',
+  });
   const app = await NestFactory.create(AppModule);
   applyMiddleware(app);
   await app.listen(process.env.PORT || 3080, () => {
