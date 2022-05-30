f8fd8e6422555623ffde5c4c1383918f84a63c4e
dja1716 <hdum@uwaterloo.ca>
2022-05-30 17:13:21 +0900

    update: cron dto

--- src/notification/dtos/cron.dto.ts
+++ src/notification/dtos/cron.dto.ts
@@ -1,4 +1,10 @@
+import { MessagingOptions } from 'firebase-admin/lib/messaging/messaging-api';
 export class CronInput {
   name: string;
   time: string;
+}
+
+export class INotificationOptions {
+  cronInput?: CronInput;
+  fcmOptions?: MessagingOptions;
 }
