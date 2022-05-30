714f838c0a2f24bfe4331304486b961c85935378
dja1716 <hdum@uwaterloo.ca>
2022-05-30 14:48:32 +0900

    notification service: send notifications

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,45 @@
+import { CoreOutput } from '@common/common.interface';
 import { Injectable } from '@nestjs/common';
+import {
+  MessagingOptions,
+  MessagingPayload,
+} from 'firebase-admin/lib/messaging/messaging-api';
+import * as admin from 'firebase-admin';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private schedulerRegistry: SchedulerRegistry) {}
+
+  async sendNotifications(
+    registrationTokenOrTokens: string | string[],
+    payload: MessagingPayload,
+    options?: MessagingOptions,
+    scheduledTime?: string,
+  ): Promise<CoreOutput> {
+    try {
+      if (!registrationTokenOrTokens) {
+        return { ok: false, error: 'No Firebase Token' };
+      }
+      if (scheduledTime) {
+        const job = new CronJob(scheduledTime, async () => {
+          await admin
+            .messaging()
+            .sendToDevice(registrationTokenOrTokens, payload, options);
+        });
+
+        this.schedulerRegistry.addCronJob('notification', job);
+        job.start();
+      } else {
+        await admin
+          .messaging()
+          .sendToDevice(registrationTokenOrTokens, payload, options);
+      }
+
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error };
+    }
+  }
+}
