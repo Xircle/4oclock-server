ce47080e94740f71f5ed8244121642d010421242
dja1716 <hdum@uwaterloo.ca>
2022-06-14 16:53:11 +0900

    edit: party entity

--- src/party/entities/party.entity.ts
+++ src/party/entities/party.entity.ts
@@ -19,4 +19,28 @@ export class Place {
 
   @Column({ default: false })
   isClosed: boolean;
+
+  @Column({ length: 511 })
+  description: string;
+
+  @Column({ length: 255 })
+  address: string;
+
+  @Column({ length: 255 })
+  placeName: string;
+
+  @Column({ length: 511, nullable: true })
+  externalLink?: string;
+
+  @Column({ length: 124 })
+  fee: string;
+
+  @Column({ length: 1023, default: [] })
+  participatingRecommendations: string[];
+
+  @Column({ length: 1023, default: '입력된 부분이 없습니다' })
+  invitationDetail: string;
+
+  @Column({ length: 1023, default: '입력된 부분이 없습니다' })
+  InvitaionInstruction: string;
 }
