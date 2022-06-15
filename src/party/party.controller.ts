ca8e1af729d4302912d6081f818915c00703f47f
dja1716 <hdum@uwaterloo.ca>
2022-06-15 17:03:59 +0900

    edit: party controller base

--- src/party/party.controller.ts
+++ src/party/party.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { User } from './../user/entities/user.entity';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { RolesGuard } from './../auth/guard/roles.guard';
+import { Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Roles } from '@auth/roles.decorator';
+import { ApiConsumes, ApiOperation } from '@nestjs/swagger';
+import { GetUser } from '@auth/decorators/get-user.decorator';
 
 @Controller('party')
-export class PartyController {}
+export class PartyController {
+  @Post('')
+  @ApiOperation({ summary: '장소 생성하기' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      {
+        name: 'images',
+        maxCount: 16,
+      },
+    ]),
+  )
+  @UseGuards(RolesGuard)
+  @Roles(['Owner'])
+  async createParty(@GetUser() authUser: User) {}
+}
