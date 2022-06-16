10b916794abce3176acfc18b8c48d0a8d0b1f4bf
dja1716 <hdum@uwaterloo.ca>
2022-06-16 16:31:15 +0900

    edit: party service

--- src/party/party.service.ts
+++ src/party/party.service.ts
@@ -1,3 +1,4 @@
+import { PartyRepository } from './repositories/party.repository';
 import { CreatePartyInput, PartyPhotoInput } from './dtos/create-party.dto';
 import { User } from '@user/entities/user.entity';
 import { CoreOutput } from '@common/common.interface';
@@ -5,11 +6,12 @@ import { NotificationService } from 'notification/notification.service';
 import { ReservationRepository } from '@reservation/repository/reservation.repository';
 import { S3Service } from '@aws/s3/s3.service';
 import { Injectable } from '@nestjs/common';
+import { getManager } from 'typeorm';
 
 @Injectable()
 export class PartyService {
   constructor(
-    private reservationRepository: ReservationRepository,
+    private partyRepository: PartyRepository,
     private s3Service: S3Service,
     private notificationService: NotificationService,
   ) {}
@@ -38,6 +40,28 @@ export class PartyService {
           imageS3Urls.push(s3_url);
         }
       }
+      //   Transction start
+      //   await getManager().transaction(async (transactionalEntityManager) => {
+      //     //   Create place
+      //     const party = await this.partyRepository.createAndSavePlace(
+      //       {
+      //         kakaoPlaceId: placeId,
+      //         coverImage: coverImageS3Url,
+      //         subImages: subImageS3Urls,
+      //         placeType,
+      //         name,
+      //         startDateAt,
+      //         team,
+      //         recommendation,
+      //         creator: authUser,
+      //       },
+      //       transactionalEntityManager,
+      //     );
+      //   });
+
+      return {
+        ok: true,
+      };
     } catch (error) {}
     return { ok: true };
   }
