dbd9ba8769fb73baa56aaa5ea347dfe6b68bd1f7
dja1716 <hdum@uwaterloo.ca>
2022-06-16 13:59:47 +0900

    edit: party controller

--- src/party/party.controller.ts
+++ src/party/party.controller.ts
@@ -1,15 +1,39 @@
+import { PartyService } from './party.service';
+import { CreatePartyInput, PartyPhotoInput } from './dtos/create-party.dto';
+import { JwtAuthGuard } from '@auth/guard/jwt-auth.guard';
+import { CoreOutput } from '@common/common.interface';
 import { User } from './../user/entities/user.entity';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { RolesGuard } from './../auth/guard/roles.guard';
-import { Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Roles } from '@auth/roles.decorator';
-import { ApiConsumes, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { GetUser } from '@auth/decorators/get-user.decorator';
 
+@ApiTags('Party')
+@ApiBearerAuth('jwt')
+@ApiOkResponse()
+@ApiUnauthorizedResponse()
+@UseGuards(JwtAuthGuard)
 @Controller('party')
 export class PartyController {
+  constructor(private partyService: PartyService) {}
   @Post('')
-  @ApiOperation({ summary: '장소 생성하기' })
+  @ApiOperation({ summary: '파티 생성하기' })
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(
     FileFieldsInterceptor([
@@ -20,6 +44,13 @@ export class PartyController {
     ]),
   )
   @UseGuards(RolesGuard)
-  @Roles(['Owner'])
-  async createParty(@GetUser() authUser: User) {}
+  @Roles(['Client', 'Admin', 'Owner'])
+  async createParty(
+    @GetUser() authUser: User,
+    @Body() createPartyInput: CreatePartyInput,
+    @UploadedFiles() files: PartyPhotoInput,
+  ): Promise<CoreOutput> {
+    console.log(createPartyInput);
+    return this.partyService.createParty(authUser, createPartyInput, files);
+  }
 }
