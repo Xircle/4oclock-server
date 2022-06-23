9c813e938a178c26c8134dfb31e76522666d0e93
dja1716 <hdum@uwaterloo.ca>
2022-06-23 12:25:24 +0900

    edit: make reservation dto - remove isVaccinated

--- src/reservation/dtos/make-reservation.dto.ts
+++ src/reservation/dtos/make-reservation.dto.ts
@@ -10,13 +10,6 @@ export class MakeReservationDto {
   @IsNotEmpty()
   @IsUUID()
   placeId: string;
-
-  @ApiProperty({
-    name: 'isVaccinated',
-    description: '백신 2차 접종 유무',
-  })
-  @IsNotEmpty()
-  isVaccinated: boolean;
 }
 
 export class MakeReservationOutput extends CoreOutput {}
