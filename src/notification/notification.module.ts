12638c83678a10fb6284a8f4a378c21b86dc1dd3
dja1716 <hdum@uwaterloo.ca>
2022-06-27 14:09:37 +0900

    fix: notification module - dependencies issue

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,14 +1,8 @@
-import { UserRepository } from '@user/repositories/user.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
-import { NotificationController } from './notification.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [NotificationService],
   exports: [NotificationService],
-  controllers: [NotificationController],
 })
 export class NotificationModule {}
