8b461a59594b86cdcb9283c3f2a4675030bb4528
dja1716 <hdum@uwaterloo.ca>
2022-06-27 11:57:49 +0900

    edit: notification module - typeorm for feature user repository

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,12 @@
+import { UserRepository } from '@user/repositories/user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [NotificationService],
   exports: [NotificationService],
   controllers: [NotificationController],
