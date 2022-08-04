10635a49f92a4962f872a3b747ef4b1277b3f5b1
dja1716 <hdum@uwaterloo.ca>
2022-08-04 16:59:12 +0900

    edit: user repository

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -1,5 +1,7 @@
+import { Place } from '@place/entities/place.entity';
+import { Reservation } from '@reservation/entities/reservation.entity';
 import { Team } from 'team/entities/team.entity';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getConnection } from 'typeorm';
 import { User } from './../entities/user.entity';
 
 @EntityRepository(User)
@@ -34,5 +36,42 @@ export class UserRepository extends Repository<User> {
       });
 
     return qb.getOne();
+  }
+
+  public async getPointThisSeason(
+    authUser: User,
+    season: number,
+  ): Promise<number> {
+    const myTeamCount = await getConnection()
+      .createQueryBuilder()
+      .select('reservation')
+      .from(Reservation, 'reservation')
+      .addFrom(Place, 'place')
+      .addFrom(Team, 'team')
+      .where('reservation.place_id = place.id')
+      .andWhere('reservation.is_canceled = false')
+      .andWhere('place.place_type = :type', { type: 'Regular-meeting' })
+      .andWhere('reservation.user_id = :userId', { userId: authUser.id })
+      .andWhere('place.team_id = :teamId', { teamId: authUser.team_id })
+      .andWhere('team.id = place.team_id')
+      .andWhere('team.season = :season', { season: season })
+      .getCount();
+
+    const otherTeamCount = await getConnection()
+      .createQueryBuilder()
+      .select('reservation')
+      .from(Reservation, 'reservation')
+      .addFrom(Place, 'place')
+      .addFrom(Team, 'team')
+      .where('reservation.place_id = place.id')
+      .andWhere('reservation.is_canceled = false')
+      .andWhere('place.place_type = :type', { type: 'Regular-meeting' })
+      .andWhere('reservation.user_id = :userId', { userId: authUser.id })
+      .andWhere('place.team_id != :teamId', { teamId: authUser.team_id })
+      .andWhere('team.id = place.team_id')
+      .andWhere('team.season = :season', { season: season })
+      .getCount();
+
+    return myTeamCount + otherTeamCount / 2;
   }
 }
