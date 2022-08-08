3f13cbfa29253c512d7cd2fb823f314d2e300961
dja1716 <hdum@uwaterloo.ca>
2022-08-08 16:44:33 +0900

    edit: main - moment config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 
 async function bootstrap() {
+  const momentConfig = require('moment');
+  require('moment-timezone');
+  momentConfig.tz.setDefault('Asia/Seoul');
+
   const firebaseConfig: ServiceAccount = {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
