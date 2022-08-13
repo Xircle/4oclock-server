7ea65082ae7761bf6078164cd79600227c5e4a53
dja1716 <hdum@uwaterloo.ca>
2022-08-13 17:35:29 +0900

    androidReccomendedVersion

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 
-
 @Controller()
 export class AppController {
   constructor() {}
@@ -13,10 +12,10 @@ export class AppController {
   @Get('version')
   getVersion() {
     return {
-      androidRecommendedVersion: 10,
-      androidMinimumVersion: 10,
-      iOSRecommendedVersion: 10,
-      iOSMinimumVersion: 10,
+      androidRecommendedVersion: 17,
+      androidMinimumVersion: 17,
+      iOSRecommendedVersion: 17,
+      iOSMinimumVersion: 17,
     };
   }
 }
