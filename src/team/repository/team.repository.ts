90a153250b1c17281a46180b9de73b455d41ecd8
dja1716 <hdum@uwaterloo.ca>
2022-08-17 14:53:26 +0900

    edit: team repository

--- src/team/repository/team.repository.ts
+++ src/team/repository/team.repository.ts
@@ -3,8 +3,9 @@ import { Team } from '../entities/team.entity';
 
 @EntityRepository(Team)
 export class TeamRepository extends Repository<Team> {
-  public async findManyTeams(): Promise<Team[]> {
+  public async findManyTeams(season = 1): Promise<Team[]> {
     return this.find({
+      where: { season: season },
       order: {
         id: 'ASC',
       },
