c9bffd4a84cd9ec30bf976b3d32267642f8b0057
dja1716 <hdum@uwaterloo.ca>
2022-09-19 21:51:44 +0900

    edit: edit application dto

--- src/application/dtos/edit-application.dto.ts
+++ src/application/dtos/edit-application.dto.ts
@@ -1,8 +1,17 @@
+import { ApplicationStatus } from './../../application/entities/application.entity';
 import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { CreateApplicationInput } from './create-application.dto';
 export class EditApplicationInput {
   @IsNotEmpty()
   @IsString()
   applicationId: string;
+
+  @ApiProperty({
+    name: '지원 상태',
+    example: 'Pending',
+  })
+  @IsEnum(ApplicationStatus)
+  @IsOptional()
+  status?: ApplicationStatus;
 }
