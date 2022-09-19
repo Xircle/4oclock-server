fc514ac62fe73d1fc8e577d100de13a1263b2507
dja1716 <hdum@uwaterloo.ca>
2022-09-19 18:51:26 +0900

    edit: create application dto

---
+++ src/application/dtos/create-application.dto.ts
@@ -0,0 +1,15 @@
+import { ApplicationStatus } from '../entities/application.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateApplicationInput {
+  @ApiProperty({
+    name: '지원 상태',
+    example: 'Pending',
+  })
+  @IsEnum(ApplicationStatus)
+  status?: ApplicationStatus;
+
+  @IsNotEmpty()
+  teamId: number;
+}
