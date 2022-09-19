a76d7d97959f9f9f51e4cf76761273c7cf582b25
dja1716 <hdum@uwaterloo.ca>
2022-09-19 19:28:26 +0900

    edit: application module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,8 +1,11 @@
+import { ApplicationRepository } from './repositories/application.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ApplicationRepository])],
   controllers: [ApplicationController],
   providers: [ApplicationService],
 })
