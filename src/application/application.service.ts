182230eacaf4f56dd7a9d6a05735cbda5bf0f72b
dja1716 <hdum@uwaterloo.ca>
2022-09-19 19:28:41 +0900

    edit: application service

--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { CreateApplicationInput } from './dtos/create-application.dto';
+import { User } from '@user/entities/user.entity';
+import { CoreOutput } from './../common/common.interface';
+import { ApplicationRepository } from './repositories/application.repository';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
-export class ApplicationService {}
+export class ApplicationService {
+  constructor(private readonly applicationRepository: ApplicationRepository) {}
+  async createApplication(
+    authUser: User,
+    { teamId }: CreateApplicationInput,
+  ): Promise<CoreOutput> {
+    try {
+      return {
+        ok: true,
+      };
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException();
+    }
+  }
+}
