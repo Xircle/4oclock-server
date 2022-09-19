492f4d188163116d5190e707292967951f21525d
dja1716 <hdum@uwaterloo.ca>
2022-09-19 17:18:03 +0900

    edit: applications entity

---
+++ src/applications/entities/applications.entity.ts
@@ -0,0 +1,59 @@
+import { Team } from 'team/entities/team.entity';
+import { User } from './../../user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum ApplicationStatus {
+  Approved = 'Approved',
+  Disapproved = 'Disapproved',
+  Enrolled = 'Enrolled',
+  Pending = 'Pending',
+}
+
+@Entity({ name: 'applications' })
+export class Applications {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'enum',
+    enum: ApplicationStatus,
+    default: ApplicationStatus.Pending,
+  })
+  status: ApplicationStatus;
+
+  @ManyToOne((type) => User, {
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'user_id' })
+  applicant?: User;
+
+  @Column('uuid')
+  user_id: string;
+
+  @ManyToOne((type) => Team, {
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'team_id' })
+  team?: Team;
+
+  @Column('integer')
+  team_id: string;
+
+  @Column('timestamptz')
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column('timestamptz', { select: false })
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
