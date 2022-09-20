a3efb878a5a08c5d12b403fbdf4ef26ba14693cf
dja1716 <hdum@uwaterloo.ca>
2022-09-20 13:55:29 +0900

    edit: get teams dto

--- src/team/dtos/get-teams.dto.ts
+++ src/team/dtos/get-teams.dto.ts
@@ -3,6 +3,12 @@ import { CoreOutput } from '@common/common.interface';
 export class TeamData {
   id: number;
   name: string;
+  season?: number;
+  leader_id?: string;
+  startDtate?: Date;
+  endDate?: Date;
+  description?: string;
+  images?: string[];
 }
 
 export class GetTeamsOutput extends CoreOutput {
