8bfa15bf5782baca44b5d907c566aaf4af2a5f99
dja1716 <hdum@uwaterloo.ca>
2022-09-20 12:58:51 +0900

    edit: get team by id

--- src/team/dtos/get-team-by-id.dto.ts
+++ src/team/dtos/get-team-by-id.dto.ts
@@ -1,8 +1,8 @@
 import { CoreOutput } from './../../common/common.interface';
 export class GetTeamByIdInput {
-  teamId: string;
+  teamId?: string;
 
-  userId: string;
+  userId?: string;
 }
 
 export class GetTeamByIdOutput extends CoreOutput {}
