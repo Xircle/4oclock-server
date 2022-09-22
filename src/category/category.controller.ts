5c41e4475b74c37d5cfae4ef33179226c7539881
dja1716 <hdum@uwaterloo.ca>
2022-09-22 14:05:40 +0900

    edit: category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,15 @@
 import { CategoryService } from './category.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { CoreOutput } from '@common/common.interface';
 
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
+
+  @Get('/all/brief')
+  @ApiOperation({ summary: '카테고리 보기' })
+  async seeAllBrief(): Promise<CoreOutput> {
+    return { ok: true };
+  }
 }
