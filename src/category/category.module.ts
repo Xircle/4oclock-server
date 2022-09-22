2f14e0c3ad3d87c1a620e3817d30c698f0ce01ba
dja1716 <hdum@uwaterloo.ca>
2022-09-22 14:11:52 +0900

    edit: category module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,12 @@
+import { CategoryRepository } from './repositories/category.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryRepository])],
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryService],
 })
 export class CategoryModule {}
