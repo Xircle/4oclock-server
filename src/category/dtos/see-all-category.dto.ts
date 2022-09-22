b614fbc1b122f853b14ceea66b28ac38294caeb7
dja1716 <hdum@uwaterloo.ca>
2022-09-22 14:40:26 +0900

    edit: see all category dto

--- src/category/dtos/see-all-category.dto.ts
+++ src/category/dtos/see-all-category.dto.ts
@@ -1,2 +1,9 @@
 import { CoreOutput } from './../../common/common.interface';
+
+export class CategoryData {
+  id: string;
+  name: string;
+  image?: string;
+}
+
 export class SeeAllCategory extends CoreOutput {}
