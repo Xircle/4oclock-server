cafb98a18930502b9c252263f6348cf3a4499303
dja1716 <hdum@uwaterloo.ca>
2022-09-22 14:34:39 +0900

    edit: category service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,9 @@
+import { CategoryRepository } from './repositories/category.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  private async seeCategoriesBrief() {}
+}
