2fbaa9dfebef0096f7cc30f1ef7cb65adc08bcb0
dja1716 <hdum@uwaterloo.ca>
2022-09-22 14:18:38 +0900

    edit: category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -8,8 +8,14 @@ export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get('/all/brief')
-  @ApiOperation({ summary: '카테고리 보기' })
+  @ApiOperation({ summary: '카테고리 간략히 보기' })
   async seeAllBrief(): Promise<CoreOutput> {
+    return { ok: true };
+  }
+
+  @Get('/all/detail')
+  @ApiOperation({ summary: '카테고리 자세히 보기' })
+  async seeAllDetail(): Promise<CoreOutput> {
     return { ok: true };
   }
 }
