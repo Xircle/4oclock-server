48bad8cbb1be29a5d85e72b48a690b1ab5420239
dja1716 <hdum@uwaterloo.ca>
2022-09-26 20:43:04 +0900

    edit: area repository

--- src/area/repositories/area.repository.ts
+++ src/area/repositories/area.repository.ts
@@ -2,4 +2,4 @@ import { Area } from './../entities/area.entity';
 import { EntityRepository, Repository, FindManyOptions } from 'typeorm';
 
 @EntityRepository(Area)
-export class CategoryRepository extends Repository<Area> {}
+export class AreaRepository extends Repository<Area> {}
