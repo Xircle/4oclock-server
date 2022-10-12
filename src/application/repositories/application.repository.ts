491526368961547f48c6c958cc2be386a9f4b926
dja1716 <hdum@uwaterloo.ca>
2022-10-12 12:52:17 +0900

    refactor: application repo - findApplicationsByStatus

--- src/application/repositories/application.repository.ts
+++ src/application/repositories/application.repository.ts
@@ -15,107 +15,76 @@ import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity
 
 @EntityRepository(Application)
 export class ApplicationRepository extends Repository<Application> {
+  public async findByStatus(
+    userId: string,
+    status: ApplicationStatus,
+    isCanceled: boolean = false,
+  ): Promise<Application[]> {
+    const data = await this.find({
+      where: {
+        user_id: userId,
+        status: status,
+        isCanceled: false,
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+      relations: ['team'],
+    });
+    return data;
+  }
+
+  convertFromRawApplications(applications: Application[]): MyApplication[] {
+    const converted: MyApplication[] = [];
+
+    for (let application of applications) {
+      converted.push({
+        id: application.id,
+        teamName: application.team.name,
+        status: application.status,
+        teamId: application.team.id,
+        appliedAt: application.createdAt,
+        isCanceled: application.isCanceled,
+        paid: application.paid,
+        teamImage: application.team.images[0],
+      });
+    }
+
+    return converted;
+  }
+
   public async findApplicationsByStatus(
     userId: string,
   ): Promise<MyApplicationsByStatus> {
-    const approveds = await this.find({
-      where: {
-        user_id: userId,
-        status: ApplicationStatus.Approved,
-      },
-      order: {
-        createdAt: 'DESC',
-      },
-      relations: ['team'],
-    });
-    const disapproveds = await this.find({
-      where: {
-        user_id: userId,
-        status: ApplicationStatus.Disapproved,
-      },
-      order: {
-        createdAt: 'DESC',
-      },
-      relations: ['team'],
-    });
-    const enrolleds = await this.find({
-      where: {
-        user_id: userId,
-        status: ApplicationStatus.Enrolled,
-      },
-      order: {
-        createdAt: 'DESC',
-      },
-      relations: ['team'],
-    });
+    const approveds = await this.findByStatus(
+      userId,
+      ApplicationStatus.Approved,
+      false,
+    );
+    const disapproveds = await this.findByStatus(
+      userId,
+      ApplicationStatus.Disapproved,
+      false,
+    );
+    const enrolleds = await this.findByStatus(
+      userId,
+      ApplicationStatus.Enrolled,
+      false,
+    );
+    const pendings = await this.findByStatus(
+      userId,
+      ApplicationStatus.Pending,
+      false,
+    );
 
-    const pendings = await this.find({
-      where: {
-        user_id: userId,
-        status: ApplicationStatus.Pending,
-      },
-      order: {
-        createdAt: 'DESC',
-      },
-      relations: ['team'],
-    });
-
-    const approvedsConverted: MyApplication[] = [];
-    const disapprovedsConverted: MyApplication[] = [];
-    const enrolledsConverted: MyApplication[] = [];
-    const pendingsConverted: MyApplication[] = [];
-
-    for (let approved of approveds) {
-      approvedsConverted.push({
-        id: approved.id,
-        teamName: approved.team.name,
-        status: approved.status,
-        teamId: approved.team.id,
-        appliedAt: approved.createdAt,
-        isCanceled: approved.isCanceled,
-        paid: approved.paid,
-        teamImage: approved.team.images[0],
-      });
-    }
-
-    for (let disapproved of disapproveds) {
-      disapprovedsConverted.push({
-        id: disapproved.id,
-        teamName: disapproved.team.name,
-        status: disapproved.status,
-        teamId: disapproved.team.id,
-        appliedAt: disapproved.createdAt,
-        isCanceled: disapproved.isCanceled,
-        paid: disapproved.paid,
-        teamImage: disapproved.team.images[0],
-      });
-    }
-
-    for (let pending of pendings) {
-      pendingsConverted.push({
-        id: pending.id,
-        teamName: pending.team.name,
-        status: pending.status,
-        teamId: pending.team.id,
-        appliedAt: pending.createdAt,
-        isCanceled: pending.isCanceled,
-        paid: pending.paid,
-        teamImage: pending.team.images[0],
-      });
-    }
-
-    for (let enrolled of enrolleds) {
-      enrolledsConverted.push({
-        id: enrolled.id,
-        teamName: enrolled.team.name,
-        status: enrolled.status,
-        teamId: enrolled.team.id,
-        appliedAt: enrolled.createdAt,
-        isCanceled: enrolled.isCanceled,
-        paid: enrolled.paid,
-        teamImage: enrolled.team.images[0],
-      });
-    }
+    const approvedsConverted: MyApplication[] =
+      this.convertFromRawApplications(approveds);
+    const disapprovedsConverted: MyApplication[] =
+      this.convertFromRawApplications(disapproveds);
+    const enrolledsConverted: MyApplication[] =
+      this.convertFromRawApplications(enrolleds);
+    const pendingsConverted: MyApplication[] =
+      this.convertFromRawApplications(pendings);
 
     return {
       approveds: approvedsConverted,
