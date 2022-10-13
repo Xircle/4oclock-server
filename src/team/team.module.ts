09823b0ca5f4491d6596167968d113c369eb9f5f
dja1716 <hdum@uwaterloo.ca>
2022-10-13 18:51:41 +0900

    edit: team module

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../auth/auth.module';
 import { S3Service } from './../aws/s3/s3.service';
 import { UserRepository } from './../user/repositories/user.repository';
 import { Module } from '@nestjs/common';
@@ -8,7 +9,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TeamRepository, Team, UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([TeamRepository, Team, UserRepository]),
+    AuthModule,
+  ],
 
   providers: [TeamService, S3Service],
   controllers: [TeamController],
