56af6216794e6e72f3da04648ca64b8d0d02323f
dja1716 <hdum@uwaterloo.ca>
2022-10-14 19:19:56 +0900

    edit: get application dto

--- src/application/dtos/get-application-by-leader.dto.ts
+++ src/application/dtos/get-application-by-leader.dto.ts
@@ -1,5 +1,13 @@
 import { CoreOutput } from '@common/common.interface';
-export class GetApplicationByLeaderData {}
+export class GetApplicationByLeaderData {
+  applicationId: string;
+  username: string;
+  mbti?: string;
+  shortBio?: string;
+  personality?: string;
+  phoneNumber?: string;
+  content?: string;
+}
 
 export class GetApplicationByLeaderOutput extends CoreOutput {
   data?: GetApplicationByLeaderData;
