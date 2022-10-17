3fd726ee04fe962980889bc8cb2093ab07c37fa5
dja1716 <hdum@uwaterloo.ca>
2022-10-17 19:53:17 +0900

    edit: get teams with filter dto

--- src/team/dtos/get-teams-with-filter.dto.ts
+++ src/team/dtos/get-teams-with-filter.dto.ts
@@ -1,4 +1,10 @@
+import { Team } from 'team/entities/team.entity';
 export class GetTeamsWithFilterInput {
   minAge?: number;
   maxAge?: number;
+}
+
+export class FilterTeam extends Team {
+  approvedCount: number;
+  applyCount: number;
 }
