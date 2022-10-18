b5a249e9cc7c89c13d2644ca80756f6b1fd27fd6
dja1716 <hdum@uwaterloo.ca>
2022-10-18 20:48:12 +0900

    edit: registed-phone-number repository

--- src/user/repositories/registed-phone-number.repository.ts
+++ src/user/repositories/registed-phone-number.repository.ts
@@ -1,4 +1,27 @@
-import { RegisteredPhoneNumber } from './../entities/registed-phone-number.entity';
+import { UserRole } from '@user/entities/user.entity';
+import { RegisteredPhoneNumber } from '../entities/registered-phone-number.entity';
 import { EntityRepository, Repository } from 'typeorm';
+
+export class VerifyByPhoneNumberOutput {
+  ok: boolean;
+  role?: UserRole;
+}
 @EntityRepository(RegisteredPhoneNumber)
-export class RegisteredPhoneNumberRepository extends Repository<RegisteredPhoneNumber> {}
+export class RegisteredPhoneNumberRepository extends Repository<RegisteredPhoneNumber> {
+  public async verifyByPhoneNumber(
+    phoneNumber: string,
+  ): Promise<VerifyByPhoneNumberOutput> {
+    const row = await this.findOne({ number: phoneNumber });
+
+    if (row) {
+      const result = {
+        ok: true,
+        role: row.role,
+      };
+      await this.delete({ number: phoneNumber });
+      return result;
+    } else {
+      return { ok: false };
+    }
+  }
+}
