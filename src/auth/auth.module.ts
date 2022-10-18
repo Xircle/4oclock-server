8958f3affbf46a8fb8eb467e2792bd3a54581856
dja1716 <hdum@uwaterloo.ca>
2022-10-18 22:02:42 +0900

    edit: RegistedPhoneNumberRepository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,3 @@
-import { RegisteredPhoneNumberRepository } from './../user/repositories/registed-phone-number.repository';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -12,12 +11,7 @@ import { UserRepository } from '@user/repositories/user.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      UserRepository,
-      SocialAccount,
-      UserProfile,
-      RegisteredPhoneNumberRepository,
-    ]),
+    TypeOrmModule.forFeature([UserRepository, SocialAccount, UserProfile]),
     JwtModule.registerAsync({
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET_KEY'),
