5792effb5cba4fc7d8b2c02d7eac7aa819b88be3
dja1716 <hdum@uwaterloo.ca>
2022-10-21 21:18:35 +0900

    edit: user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -41,21 +41,23 @@ import { Roles } from '@auth/roles.decorator';
 import { GetMyTeamsLeaderOutput } from './dtos/get-my-teams-leader.dto';
 
 @ApiTags('User')
-@ApiBearerAuth('jwt')
 @ApiOkResponse()
 @ApiUnauthorizedResponse()
-@UseGuards(JwtAuthGuard)
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   @ApiOperation({ summary: '유저 정보 ' })
   async me(@GetUser() authUser: User) {
     return this.userService.me(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Patch('me/verify/:code')
   @ApiOperation({ summary: '유저 활동코드 입력' })
   async VerifyByCode(
@@ -65,6 +67,8 @@ export class UserController {
     return this.userService.verifyUserByCode(authUser, code);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Patch('me/team/:teamId')
   async patchTeam(
     @GetUser() authUser: User,
@@ -90,12 +94,16 @@ export class UserController {
     );
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Delete('/delete')
   @ApiOperation({ summary: '유저 삭제' })
   async deleteUser(@GetUser() authUser: User): Promise<CoreOutput> {
     return this.userService.deleteUser(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('/profile/random')
   @ApiOperation({ summary: '랜덤 유저 프로필 조회' })
   @ApiQuery({
@@ -118,12 +126,16 @@ export class UserController {
     return this.userService.seeUserById(id);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('history')
   @ApiOperation({ summary: '유저가 신청한 모임 조회' })
   async getMyPlace(@GetUser() authUser: User): Promise<GetMyPlaceOutput> {
     return this.userService.getMyPlace(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('history/created')
   @ApiOperation({ summary: '유저가 생성한 모임 조회' })
   async getMyPlaceCreated(
@@ -132,6 +144,8 @@ export class UserController {
     return this.userService.getMyPlaceCreated(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('history/applications')
   @ApiOperation({ summary: '유저가 지원한 팀 조회' })
   async getMyApplications(
@@ -140,6 +154,8 @@ export class UserController {
     return this.userService.getMyApplications(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('leader/myteams')
   @Roles(['Owner'])
   @ApiOperation({ summary: '유저가 담당하는 팀들 조회' })
@@ -149,17 +165,23 @@ export class UserController {
     return this.userService.getMyTeamsLeader(authUser);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('point')
   async getPoint(@GetUser() authUser: User): Promise<GetPointOutput> {
     return this.userService.getPoint(authUser, 1);
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Get('report/:userId')
   @ApiOperation({ summary: '특정 유저 신고하기' })
   async reportUser(@GetUser() authUser: User) {
     return this.userService.reportUser();
   }
 
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard)
   @Post('/fbtoken/:token')
   @ApiOperation({ summary: 'for testing' })
   async updateFirebaseToken(
