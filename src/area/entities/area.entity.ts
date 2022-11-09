c2c4adc0c19680d18083c1395bc552313d8d4c72
dja1716 <hdum@uwaterloo.ca>
2022-11-09 14:51:03 +0900

    clean up: area entity

--- src/area/entities/area.entity.ts
+++ src/area/entities/area.entity.ts
@@ -1,5 +1,4 @@
-import { Team } from './../../team/entities/team.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'areas' })
 export class Area {
