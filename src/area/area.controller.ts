e2d31561c9f34e303c2cdf6726e832ff50e0a4da
dja1716 <hdum@uwaterloo.ca>
2022-12-16 02:43:16 +0900

    edit: area controller

--- src/area/area.controller.ts
+++ src/area/area.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { GetAreaOutput } from './dtos/get-areas.dto';
+import { AreaService } from './area.service';
+import { Controller, Get } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Area')
+@ApiOkResponse()
+@ApiUnauthorizedResponse()
 @Controller('area')
-export class AreaController {}
+export class AreaController {
+  constructor(private areaService: AreaService) {}
+
+  @Get('/all')
+  async getAreas(): Promise<GetAreaOutput> {
+    return await this.areaService.getAreas();
+  }
+}
