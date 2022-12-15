66028716927b9afff9984b8d5c529d3726ef762b
dja1716 <hdum@uwaterloo.ca>
2022-12-16 02:43:29 +0900

    edit: area module

--- src/area/area.module.ts
+++ src/area/area.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AreaService } from './area.service';
 import { AreaController } from './area.controller';
+import { AreaRepository } from './repositories/area.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AreaRepository])],
   providers: [AreaService],
-  controllers: [AreaController]
+  controllers: [AreaController],
 })
 export class AreaModule {}
