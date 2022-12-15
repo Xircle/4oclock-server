6be627a81b4fc3a2c18ce54afbe12ac804eec26b
dja1716 <hdum@uwaterloo.ca>
2022-12-16 02:43:39 +0900

    edit: area service

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { AreaRepository } from './repositories/area.repository';
 
 @Injectable()
-export class AreaService {}
+export class AreaService {
+  constructor(private areaRepository: AreaRepository) {}
+  public async getAreas() {
+    try {
+      const areas = await this.areaRepository.find({ is_closed: false });
+      return { ok: true, areas: areas };
+    } catch (error) {
+      return { ok: false, error };
+    }
+  }
+}
